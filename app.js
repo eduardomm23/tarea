21971ceae6a90fb4b0135ff6713b6e860932f633
eduardomm23 <eduardo.martinez2302.eam@gmail.com>
2021-02-02 20:36:13 -0600

    este es mi primer registro

---
+++ app.js
@@ -0,0 +1,39 @@
+let myArray = [];
+
+function myFunction (){
+    const a = document.getElementById("a1").value;
+    const b = document.getElementById("a2").value;
+    const c = document.getElementById("a3").value;
+    myArray.push(a,b,c);
+
+const tabl1 = document.createElement("table");
+tabl1.border=1;
+tabl1.width=200;
+tabl1.setAttribute('border', '1');
+
+document.getElementById("result").appendChild(tabl1);
+
+myArray.map((value) => {
+
+    var tr = document.createElement('TR');
+    tabl1.appendChild(tr);
+    var td1 = document.createElement('TD');
+    td1.width = '75';
+    td1.innerHTML += value;
+    var td2 = document.createElement('TD');
+    td2.width = '75';
+    td2.innerHTML += value;
+    var td3 = document.createElement('TD');
+    td3.width = '75';
+    td3.innerHTML += value;
+    
+    
+
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3); 
+    
+    
+
+  })
+}
