da363a14436981fd7ce5bbd6c68e39ad57cf7c83
eduardomm23 <eduardo.martinez2302.eam@gmail.com>
2021-02-03 13:42:39 -0600

    tercer avance validado

--- app.js
+++ app.js
@@ -1,46 +1,52 @@
-let myArray = [];
-
-
 function myFunction (){
-    const a = document.getElementById("a1").value;
-    const b = document.getElementById("a2").value;
-    const c = document.getElementById("a3").value;
-    myArray.push(a,b,c);
-
-const tabl1 = document.createElement("table");
-tabl1.border=1;
-tabl1.width=200;
-tabl1.setAttribute('border', '3');
-
-document.getElementById("result").appendChild(tabl1);
-
-myArray.map((value) => {
+    let myArray = [];
+     const a = document.getElementById("a1").value;
+     const b = document.getElementById("a2").value;
+     const c = document.getElementById("a3").value;
+ if (a === "" && b === "" && c === "") {
+    alert ("Por favor ingresa la informacion");
+ }
+ else {
+     let resl = {
+        name: a,
+        apel: b,
+        eda: c,
+     }
+     myArray.push(resl);
+    
+ const tabl1 = document.createElement("table");
+  tabl1.border=1;
+  tabl1.width=200;
+  tabl1.setAttribute('border', '3');
+ 
+  document.getElementById("result").appendChild(tabl1);
+   var tr = document.createElement('TR');
+   tabl1.appendChild(tr);
+ 
+   myArray.map((value) => {
     var i 
-   	for (i=0;i<document.myForm.hm.length;i++){ 
-      	if (document.myForm.hm[i].checked) 
-         	break; 
-   	} 
-   	
-    var tr = document.createElement('TR');
-    tabl1.appendChild(tr);
+     for (i=0;i<document.myForm.hm.length;i++){ 
+     if (document.myForm.hm[i].checked) 
+            break; 
+         } 
     var td1 = document.createElement('TD');
     td1.width = '75';
-    td1.innerHTML += value;
+    td1.innerHTML += value.name;
     var td2 = document.createElement('TD');
     td2.width = '75';
-    td2.innerHTML += value;
+    td2.innerHTML += value.apel;
     var td3 = document.createElement('TD');
     td3.width = '75';
-    td3.innerHTML += value;
+    td3.innerHTML += value.eda;
     var td4 = document.createElement('TD');
     td4.width = '75';
     td4.innerHTML += document.myForm.hm[i].value;
-        
-
+    
     tr.appendChild(td1);
     tr.appendChild(td2);
     tr.appendChild(td3); 
-    tr.appendChild(td4); 
+    tr.appendChild(td4);   
+      })
+ }
 
-  })
 }
