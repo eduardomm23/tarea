6a08e036977d6675cfd49f350be3c8b0d9a5eb28
eduardomm23 <eduardo.martinez2302.eam@gmail.com>
2021-02-03 08:41:57 -0600

    segundo avance

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let myArray = [];
 
+
 function myFunction (){
     const a = document.getElementById("a1").value;
     const b = document.getElementById("a2").value;
@@ -9,12 +10,17 @@ function myFunction (){
 const tabl1 = document.createElement("table");
 tabl1.border=1;
 tabl1.width=200;
-tabl1.setAttribute('border', '1');
+tabl1.setAttribute('border', '3');
 
 document.getElementById("result").appendChild(tabl1);
 
 myArray.map((value) => {
-
+    var i 
+   	for (i=0;i<document.myForm.hm.length;i++){ 
+      	if (document.myForm.hm[i].checked) 
+         	break; 
+   	} 
+   	
     var tr = document.createElement('TR');
     tabl1.appendChild(tr);
     var td1 = document.createElement('TD');
@@ -26,14 +32,15 @@ myArray.map((value) => {
     var td3 = document.createElement('TD');
     td3.width = '75';
     td3.innerHTML += value;
-    
-    
+    var td4 = document.createElement('TD');
+    td4.width = '75';
+    td4.innerHTML += document.myForm.hm[i].value;
+        
 
     tr.appendChild(td1);
     tr.appendChild(td2);
     tr.appendChild(td3); 
-    
-    
+    tr.appendChild(td4); 
 
   })
 }
